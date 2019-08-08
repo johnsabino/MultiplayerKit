3ebebb7a2840da54534012b54605062b73f68dc3
jonhpol <j.paulo_os@hotmail.com>
2019-08-08 15:09:35 -0300

    First commit

---
+++ MultiplayerKit/MultiplayerKit.h
@@ -0,0 +1,19 @@
+//
+//  MultiplayerKit.h
+//  MultiplayerKit
+//
+//  Created by João Paulo de Oliveira Sabino on 08/08/19.
+//  Copyright © 2019 João Paulo de Oliveira Sabino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MultiplayerKit.
+FOUNDATION_EXPORT double MultiplayerKitVersionNumber;
+
+//! Project version string for MultiplayerKit.
+FOUNDATION_EXPORT const unsigned char MultiplayerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MultiplayerKit/PublicHeader.h>
+
+
