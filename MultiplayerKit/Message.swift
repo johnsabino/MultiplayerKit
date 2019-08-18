33a828d27d8fabb1e76ae29fe06ca3f4a2c3ea66
jonhpol <j.paulo_os@hotmail.com>
2019-08-18 01:02:47 -0300

    resolved bug in message type

--- MultiplayerKit/Message.swift
+++ MultiplayerKit/Message.swift
@@ -1,6 +1,14 @@
 public extension Data {
     @discardableResult
     func caseIs<T: MessageProtocol>(_ type: T.Type, doThis: (T) -> Void) -> Data {
+        if let set = try? JSONSerialization.jsonObject(with: self, options: []) as? [String: Any] {
+            if (set["type"] as? String) != "\(T.self)" {
+                print((set["type"] as? String), "é diferente de: \(T.self)" )
+                return self
+                
+            }
+        }
+        
         if let decoded = try? JSONDecoder().decode(T.self, from: self) {
             doThis(decoded)
         }
@@ -16,11 +24,11 @@ public extension MessageProtocol {
     
     var asDictionary: [String: Any] {
         let mirror = Mirror(reflecting: self)
-        let dict = Dictionary(uniqueKeysWithValues: mirror.children.lazy.map({ (label: String?, value: Any) -> (String, Any)? in
+        var dict = Dictionary(uniqueKeysWithValues: mirror.children.lazy.map({ (label: String?, value: Any) -> (String, Any)? in
             guard let label = label else { return nil }
             return (label, value)
         }).compactMap { $0 })
-        //dict["type"] = "\(Self.self)"
+        dict["type"] = "\(Self.self)"
         return dict
     }
     
